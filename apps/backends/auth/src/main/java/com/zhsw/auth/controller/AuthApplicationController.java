ae93e20f5a0788db4104d0b8a21c69d04f084abb
Steliyana Vasileva <133668417+ssste09@users.noreply.github.com>
2025-08-30 18:31:35 +0200

    login and sign up endpoints (#19)
    Co-authored-by: Stella Vasileva <stellavasileva@Leonardos-MacBook-Pro-2.local>

--- apps/backends/auth/src/main/java/com/zhsw/auth/controller/AuthApplicationController.java
+++ apps/backends/auth/src/main/java/com/zhsw/auth/controller/AuthApplicationController.java
@@ -2,6 +2,9 @@ package com.zhsw.auth.controller;
 
 
 import org.openapitools.api.AuthApi;
+import org.openapitools.model.LoginUserRequest;
+import org.openapitools.model.LoginUserResponse;
+import org.openapitools.model.SignUpUserRequest;
 import org.openapitools.model.User;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +14,16 @@ public class AuthApplicationController implements AuthApi {
 
     @Override
     public ResponseEntity<User> userGET(Integer userId) {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    @Override
+    public ResponseEntity<User> signup(SignUpUserRequest signUpUserRequest) {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    @Override
+    public ResponseEntity<LoginUserResponse> login(LoginUserRequest loginUserRequest) {
         throw new UnsupportedOperationException("Not implemented yet");
     }
 }
