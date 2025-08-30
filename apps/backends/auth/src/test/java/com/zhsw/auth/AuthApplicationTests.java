cc7495e4b62bad78f14507bdabef892183f8fcc0
Steliyana Vasileva <133668417+ssste09@users.noreply.github.com>
2025-08-30 12:05:17 +0200

    heloooo (#12)
    Co-authored-by: Stella Vasileva <stellavasileva@Leonardos-MacBook-Pro-2.local>

--- apps/backends/auth/src/test/java/com/zhsw/auth/AuthApplicationTests.java
+++ apps/backends/auth/src/test/java/com/zhsw/auth/AuthApplicationTests.java
@@ -1,13 +1,23 @@
 package com.zhsw.auth;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Tag("unit")
 class AuthApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void shouldFail() {
+		var ciao = "ciao";
+		assertEquals("ciao", ciao);
 	}
 
 }
