a2ccea27b465828c29076a63df897a13bfda1227
Stella Vasileva <stellavasileva@Leonardos-MacBook-Pro-2.local>
2025-08-30 11:13:14 +0200

    try pr checks

---
+++ apps/backends/auth/src/main/java/com/zhsw/auth/controller/AuthApplicationController.java
@@ -0,0 +1,22 @@
+package com.zhsw.auth.controller;
+
+
+import org.openapitools.api.AuthApi;
+import org.openapitools.model.ListUsersGET200Response;
+import org.openapitools.model.TrySpecGenerationGET200Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthApplicationController implements AuthApi {
+    @Override
+    public ResponseEntity<TrySpecGenerationGET200Response> trySpecGenerationGET() {
+        return new ResponseEntity(new TrySpecGenerationGET200Response().key("ciao"), HttpStatus.OK);
+    }
+
+    @Override
+    public ResponseEntity<ListUsersGET200Response> listUsersGET(String companyId) {
+        return AuthApi.super.listUsersGET(companyId);
+    }
+}
