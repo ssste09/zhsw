01702467b627081124e7625ac10d4d4d2af040da
Stella Vasileva <stellavasileva@Leonardos-MacBook-Pro-2.local>
2025-08-30 12:57:47 +0200

    new specs for user

--- apps/backends/auth/src/main/java/com/zhsw/auth/controller/AuthApplicationController.java
+++ apps/backends/auth/src/main/java/com/zhsw/auth/controller/AuthApplicationController.java
@@ -2,21 +2,15 @@ package com.zhsw.auth.controller;
 
 
 import org.openapitools.api.AuthApi;
-import org.openapitools.model.ListUsersGET200Response;
-import org.openapitools.model.TrySpecGenerationGET200Response;
-import org.springframework.http.HttpStatus;
+import org.openapitools.model.User;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthApplicationController implements AuthApi {
-    @Override
-    public ResponseEntity<TrySpecGenerationGET200Response> trySpecGenerationGET() {
-        return new ResponseEntity(new TrySpecGenerationGET200Response().key("ciaoooo"), HttpStatus.OK);
-    }
 
     @Override
-    public ResponseEntity<ListUsersGET200Response> listUsersGET(String companyId) {
-        return AuthApi.super.listUsersGET(companyId);
+    public ResponseEntity<User> userGET(Integer userId) {
+        throw new UnsupportedOperationException("Not implemented yet");
     }
 }
