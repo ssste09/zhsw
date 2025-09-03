6e45acd28306fb1ea8c64eb114c5f45c7a15864d
Steliyana Vasileva <133668417+ssste09@users.noreply.github.com>
2025-09-03 16:02:33 +0200

    product controller created (#28)
    Co-authored-by: Stella Vasileva <stellavasileva@Leonardos-MacBook-Pro-2.local>

---
+++ apps/backends/product/src/main/java/com/zhsw/product/controller/ProductApplicationController.java
@@ -0,0 +1,22 @@
+package com.zhsw.product.controller;
+
+import org.openapitools.api.ProductsApi;
+import org.openapitools.model.Product;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ProductApplicationController implements ProductsApi {
+
+    @Override
+    public ResponseEntity<List<Product>> getAllProducts() {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    @Override
+    public ResponseEntity<Product> getProductById(Integer productId) {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+}
