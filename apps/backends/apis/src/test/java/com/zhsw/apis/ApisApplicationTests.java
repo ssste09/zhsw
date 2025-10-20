7e7dad4785e9d09b469a6524097cb2c2289270d5
Steliyana Vasileva <133668417+ssste09@users.noreply.github.com>
2025-10-20 18:48:57 +0200

    apis service created (#38)
    Co-authored-by: Stella Vasileva <stellavasileva@Leonardos-MacBook-Pro-2.local>

---
+++ apps/backends/apis/src/test/java/com/zhsw/apis/ApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhsw.apis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
