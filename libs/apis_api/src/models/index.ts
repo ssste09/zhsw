ffa97eecbbe8a2fe79a5b756f6a0bcc2c93f9abb
Steliyana Vasileva <133668417+ssste09@users.noreply.github.com>
2026-06-27 11:46:55 +0200

    refactor(specs): Remove generated libs and package-locks from git tracking (#47)
    Co-authored-by: Stella Vasileva <stellavasileva@Leonardos-MacBook-Pro-2.local>

--- libs/apis_api/src/models/index.ts
+++
@@ -1,8 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export * from './Event';
-export * from './Events';
-export * from './Hourly';
-export * from './Location';
-export * from './Locations';
-export * from './Weather';
